5a43093e5ea41e6003a54e172cf99a5b25d28688
johan damm <johan.b.damm@gmail.com>
2015-04-20 18:19:21 +0200

    Initial commit

---
+++ app/src/main/java/com/johandamm/meridian/MainActivity.java
@@ -0,0 +1,68 @@
+package com.johandamm.meridian;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void StartScanAndSetName(View view){
+        IntentIntegrator.initiateScan(this);
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanningResult != null) {
+            String contents = scanningResult.getContents();
+            PersonContext person = GetPersonFromScanResults(contents);
+
+            personEditText.setText(person.Name, TextView.BufferType.EDITABLE);
+        }
+
+
+//retrieve scan result
+    }
+
+    private PersonContext GetPersonFromScanResults(String contents) {
+
+        return new PersonContext("mollyporph","Otto Remse","0737739799","Inget Team >:");
+    }
+
+}
+}
